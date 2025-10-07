ed0e921c803960189988d475008b98d787ccc0d3
AdityaMittal08 <mittaladitya0801@gmail.com>
2025-10-07 22:53:31 +0530

    434. Number of Segments in a String

---
+++ 434._Number_of_Segments_in_a_String.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int countSegments(string s) {
+        int count = 0;
+        s.push_back(' ');
+        for(int i = 1; i < s.size(); i++){
+            if(s[i] == ' ' && s[i-1] != ' '){
+                count++;
+            }
+        }
+        return count;
+    }
+};
