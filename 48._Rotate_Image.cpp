d33f46b593a096b634fa3c9ff3d27445612dd5cd
AdityaMittal08 <mittaladitya0801@gmail.com>
2025-11-22 10:26:32 +0530

    48. Rotate Image

---
+++ 48._Rotate_Image.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int a = 0;
+        int c = matrix.size();
+        for(int i = 0; i < n; i++){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == j){
+                    break;
+                }
+                swap(matrix[j][i], matrix[i][j]);
+            }
+        }
+
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix.size(); j++){
+                if(j > n-1-j){
+                    break;
+                }
+                swap(matrix[i][j], matrix[i][n -1- j]);
+            }
+        }
+    }
+};
