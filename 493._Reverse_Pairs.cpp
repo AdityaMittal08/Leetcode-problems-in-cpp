c1bbbcce5bcfb97873c60710be95b8030fea3820
AdityaMittal08 <mittaladitya0801@gmail.com>
2026-08-19 16:49:32 +0530

    493. Reverse Pairs

---
+++ 493._Reverse_Pairs.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int mergeSort(vector<int>& nums, vector<int>& temp, int low, int high) {
+        if (low >= high) {
+            return 0;
+        }
+
+        int mid = low + (high - low) / 2;
+        
+        int count = mergeSort(nums, temp, low, mid) + mergeSort(nums, temp, mid + 1, high);
+
+        int j = mid + 1;
+        for (int i = low; i <= mid; i++) {
+            while (j <= high && nums[i] > 2LL * nums[j]) {
+                j++;
+            }
+            count += (j - (mid + 1));
+        }
+
+        int left = low, right = mid + 1;
+        int k = low;
+
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp[k++] = nums[left++];
+            } else {
+                temp[k++] = nums[right++];
+            }
+        }
+
+        while (left <= mid) temp[k++] = nums[left++];
+        while (right <= high) temp[k++] = nums[right++];
+
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp[i];
+        }
+
+        return count;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        vector<int> temp(nums.size());
+        return mergeSort(nums, temp, 0, nums.size() - 1);
+    }
+};
