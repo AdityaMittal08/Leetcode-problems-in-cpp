c6da6f707e53132684dbb886e8ca97d9278ec3cc
AdityaMittal08 <mittaladitya0801@gmail.com>
2026-08-24 21:33:41 +0530

    Changes and written clean code

--- 206._Reverse_Linked_List.cpp
+++ 206._Reverse_Linked_List.cpp
@@ -11,17 +11,15 @@ struct ListNode {
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode * prev = nullptr;
-        ListNode * current = head;
-        ListNode * next_node = nullptr;
-        
-        while(current != nullptr){
-            next_node = current->next;
-            current-> next = prev;
+        ListNode* prev = NULL;
+        ListNode* current = head;
+        while(current != NULL){
+            ListNode* temp = current->next;
+            current->next = prev;
             prev = current;
-            current = next_node;
+            current = temp;
         }
-        
+
         return prev;
     }
 };
